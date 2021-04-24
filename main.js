9439d1bc78e6482604cdaba03115cc081418df3c
IrSg <winst1143@gmail.com>
2021-04-24 14:32:30 -0400

    update next file

--- main.js
+++ main.js
@@ -67,7 +67,7 @@ let tray = null
 app.whenReady().then(() => {
     tray = new Tray('mountains-icon.jpg')
     const contextMenu = Menu.buildFromTemplate([
-        { label: 'Item1', type: 'radio' },
+        { label: 'Copy the text', type: 'radio' },
         { label: 'Item2', type: 'radio' },
         { label: 'Item3', type: 'radio', checked: true },
         { label: 'Item4', type: 'radio' }
