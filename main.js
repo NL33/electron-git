1f8f205a6cf0c7a04acdaf25b1a798383318d3c2
IrSg <winst1143@gmail.com>
2021-08-23 11:59:42 -0400

    remove code for description window, based on dexiedb structure. New structure is to create an actual file that contains the project description

--- main.js
+++ main.js
@@ -109,30 +109,6 @@ async function saveNewVersionWindow(windowTitle) {
     */
 }
 /********************************************************* */
-/****Open Project Description Window * *************/
-
-ipcMain.on('open-description-window', (event, projectPath, projectName)=>{
-    openDescriptionWindow(projectPath, projectName)
-})
-
-function openDescriptionWindow(projectPath, projectName){
-    var descriptionWindow = new BrowserWindow({
-        //width: 700,
-        //height: 620,
-        // transparent: true,
-        //x: 415,
-        //y: 0,
-        title: 'Project Description',
-        webPreferences: {
-            additionalArguments: [projectPath, projectName],
-            nodeIntegration: true,  //set to false by default for security reasons. TO access node.js API (eg, use require(...)) in a renderer, this has to be set to true
-            contextIsolation: false, //set to true by default. False if want to use node api in renderer process,
-            enableRemoteModule: true
-        }
-    })
-
-    descriptionWindow.loadURL('file://' + __dirname + '/views/description-window.html')
-}
 
 /******* OPEN NEW WINDOW TO VIEW COMPARISONS ********/
 
